8a7f382a2fb2b5e21a180882fe7e5c713198b6ed
anshumantnt <anshumant@nashtechglobal.com>
2025-03-25 10:11:46 +0530

    fixed image render on upcoming events

--- Components/EventList/EventList.tsx
+++ Components/EventList/EventList.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -6,13 +6,13 @@ import {
   FlatList,
   StyleSheet,
   Pressable,
-  ActivityIndicator
-} from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import Footer from '../Footer/Footer';
-import { fetchEvents } from '../../Services/Events/eventService';
-import { useAuth } from '../../Context/AuthContext';
-import Toast from 'react-native-toast-message';
+  ActivityIndicator,
+} from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import Footer from "../Footer/Footer";
+import { fetchEvents } from "../../Services/Events/eventService";
+import { useAuth } from "../../Context/AuthContext";
+import Toast from "react-native-toast-message";
 
 function EventList({ navigation, route }) {
   const { user, token } = useAuth();
@@ -20,40 +20,52 @@ function EventList({ navigation, route }) {
   const [loading, setLoading] = useState(true);
   const defaultImage = require("../../assets/default_event.jpg");
 
-  useEffect(()=>{
+  useEffect(() => {
     const getEvents = async () => {
       setLoading(true);
       try {
         const data = await fetchEvents(user.id, token);
-        const trendingEvents =  data.filter(event => route.name=== "Trending Events" ? event.trending : event).map(event => {
-          const startDate = new Date(event.event_start_date);
-          const endDate = new Date(event.event_end_date);
-          const schedule = `${startDate.toLocaleString('default', { month: 'short' })} ${startDate.getDate()} | ${startDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })} - ${endDate.toLocaleString('default', { month: 'short' })} ${endDate.getDate()} | ${endDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}`;
-          const image = event.image ? { uri: `data:image/jpeg;base64,${event.image}` } : defaultImage
-          return { ...event, schedule, image };
-        });
-        setEvents(trendingEvents)
-      }
-      catch(error){
+        const trendingEvents = data
+          .filter((event) =>
+            route.name === "Trending Events" ? event.trending : event
+          )
+          .map((event) => {
+            const startDate = new Date(event.event_start_date);
+            const endDate = new Date(event.event_end_date);
+            const schedule = `${startDate.toLocaleString("default", {
+              month: "short",
+            })} ${startDate.getDate()} | ${startDate.toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+            })} - ${endDate.toLocaleString("default", {
+              month: "short",
+            })} ${endDate.getDate()} | ${endDate.toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+            })}`;
+            const image = event.image ? { uri: event.image } : defaultImage;
+            return { ...event, schedule, image };
+          });
+        setEvents(trendingEvents);
+      } catch (error) {
         Toast.show({
-          text1:"Error",
+          text1: "Error",
           text2: error.message,
-          type: 'error',
+          type: "error",
         });
-      }
-      finally {
+      } finally {
         setLoading(false);
       }
     };
 
     getEvents();
-},[token])
+  }, [token]);
 
   // Displays Event Cards
 
   const renderBanner = ({ item, index }) => {
     const EventPress = () => {
-      navigation.navigate('ViewEvent', { eventData: item });
+      navigation.navigate("ViewEvent", { eventData: item });
     };
 
     return (
@@ -62,26 +74,24 @@ function EventList({ navigation, route }) {
           <Image
             source={item.image}
             style={styles.cardImage}
-            resizeMode='cover'
+            resizeMode="cover"
           />
           <View style={styles.infoContainer}>
             <Text style={styles.eventText}>{item.event_name}</Text>
           </View>
           <View style={styles.infoContainer}>
-            <Ionicons name='calendar-outline' size={22} color='#45474D' />
+            <Ionicons name="calendar-outline" size={22} color="#45474D" />
             <Text
               style={styles.infoText}
               numberOfLines={1}
-              ellipsizeMode='tail'
+              ellipsizeMode="tail"
             >
               {item.schedule}
             </Text>
           </View>
           <View style={styles.infoContainer}>
-            <Ionicons name='location-outline' size={22} color='#45474D' />
-            <Text style={styles.infoText}>
-             {item.location}
-            </Text>
+            <Ionicons name="location-outline" size={22} color="#45474D" />
+            <Text style={styles.infoText}>{item.location}</Text>
           </View>
           <Pressable style={styles.detailBtn} onPress={EventPress}>
             <Text style={styles.btnText}>View detail</Text>
@@ -107,18 +117,18 @@ function EventList({ navigation, route }) {
         keyExtractor={(item) => item.id}
         ListHeaderComponent={() => (
           <View style={styles.listHeaderContainer}>
-            {route.name === 'Upcoming Events' ? (
+            {route.name === "Upcoming Events" ? (
               <Ionicons
-                name='calendar-outline'
+                name="calendar-outline"
                 size={25}
-                color='#e25822'
+                color="#e25822"
                 style={{ paddingVertical: 4 }}
               />
             ) : (
               <Ionicons
-                name='flame-outline'
+                name="flame-outline"
                 size={25}
-                color='#e25822'
+                color="#e25822"
                 style={{ paddingVertical: 4 }}
               />
             )}
@@ -136,90 +146,90 @@ const styles = StyleSheet.create({
   cardContainer: {
     paddingHorizontal: 15,
     paddingVertical: 10,
-    backgroundColor: '#ffffff',
+    backgroundColor: "#ffffff",
   },
   card: {
     borderWidth: 1,
-    borderColor: '#f0f1f2',
+    borderColor: "#f0f1f2",
     borderRadius: 12,
-    overflow: 'hidden',
+    overflow: "hidden",
   },
   cardImage: {
     height: 200,
-    width: 'auto',
+    width: "auto",
     marginBottom: 10,
   },
   infoContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     paddingHorizontal: 10,
     paddingVertical: 5,
   },
   infoText: {
     fontSize: 14,
-    color: '#45474D',
-    fontWeight: '600',
+    color: "#45474D",
+    fontWeight: "600",
     paddingLeft: 10,
   },
   eventText: {
     fontSize: 18,
-    color: '#000000',
-    fontWeight: '700',
+    color: "#000000",
+    fontWeight: "700",
   },
   detailBtn: {
     borderWidth: 1,
-    borderStyle: 'solid',
-    borderColor: '#d6001c',
+    borderStyle: "solid",
+    borderColor: "#d6001c",
     marginHorizontal: 10,
     marginVertical: 15,
     paddingVertical: 8,
-    backgroundColor: '#ffffff',
+    backgroundColor: "#ffffff",
     borderRadius: 10,
   },
   btnText: {
-    color: '#d6001c',
-    textAlign: 'center',
+    color: "#d6001c",
+    textAlign: "center",
     fontSize: 16,
-    fontWeight: '600',
+    fontWeight: "600",
   },
   listHeaderContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     paddingTop: 15,
     paddingBottom: 10,
     paddingHorizontal: 10,
-    backgroundColor: '#ffffff',
+    backgroundColor: "#ffffff",
     borderBottomWidth: 1,
-    borderStyle: 'solid',
-    borderBottomColor: '#f0f1f2',
+    borderStyle: "solid",
+    borderBottomColor: "#f0f1f2",
   },
   listHeader: {
     fontSize: 24,
-    fontWeight: '700',
-    color: '#e25822',
+    fontWeight: "700",
+    color: "#e25822",
     paddingHorizontal: 5,
   },
   listTotal: {
     fontSize: 24,
-    color: '#45474D',
-    fontWeight: '500',
+    color: "#45474D",
+    fontWeight: "500",
     paddingHorizontal: 5,
   },
-   loaderContainer: {
+  loaderContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
   },
   noEventsContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
     padding: 20,
   },
   noEventsText: {
     fontSize: 16,
-    color: '#45474D',
-    fontWeight: '600',
+    color: "#45474D",
+    fontWeight: "600",
   },
 });
 
